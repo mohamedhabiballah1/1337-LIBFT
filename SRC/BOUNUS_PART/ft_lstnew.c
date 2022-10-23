78dd8033cd780b4d50efc4dd811af6c6d8b65756
Mohamed HABIB ALLAH <100409109+mohamedhabiballah1@users.noreply.github.com>
2022-10-24 00:47:16 +0100

    Add files via upload

--- SRC/BOUNUS_PART/ft_lstnew.c
+++ SRC/BOUNUS_PART/ft_lstnew.c
@@ -1,12 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhabib-a <mhabib-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 12:11:37 by mhabib-a          #+#    #+#             */
-/*   Updated: 2022/10/22 12:11:38 by mhabib-a         ###   ########.fr       */
+/*   Updated: 2022/10/24 00:41:40 by mhabib-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include"libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = malloc(sizeof(t_list));
+	new -> content = content;
+	new -> next = NULL;
+	return (new);
+}
