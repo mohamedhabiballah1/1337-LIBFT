06cdf1c9577ad1da3ae902165406846152b5b7c3
Mohamed HABIB ALLAH <100409109+mohamedhabiballah1@users.noreply.github.com>
2022-11-05 13:42:41 +0100

    Add files via upload

--- SRC/BOUNUS_PART/ft_lstnew.c
+++ SRC/BOUNUS_PART/ft_lstnew.c
@@ -12,12 +12,13 @@
 
 #include"libft.h"
 
-t_list	*ft_lstnew(void *content)
+t_list  *ft_lstnew(void *content)
 {
-	t_list	*new;
-
-	new = malloc(sizeof(t_list));
-	new -> content = content;
-	new -> next = NULL;
-	return (new);
+    t_list  *new;
+    new = malloc(sizeof(t_list));
+    if (new == NULL)
+        return (NULL);
+    new -> content = content;
+    new -> next = NULL;
+    return (new);
 }
